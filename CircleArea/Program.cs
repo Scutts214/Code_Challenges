a426ef44547e38b9439226cf52f538a9211d82f9
Stephen Cutts <sbcutts1981@icloud.com>
2024-06-25 10:02:39 -0700

    C# Calculate area of a circle

---
+++ CircleArea/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CircleArea;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Caluculate radius of a circle");
+        Console.Write("Enter Radius: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        
+        var area = Math.PI * radius * radius;
+        Console.WriteLine($"The area of the circleis {area}");
+    }
+}
