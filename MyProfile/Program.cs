2b04df1cc8b3469b9ed15a071a1d2d8fc4fb586c
Stephen Cutts <sbcutts1981@icloud.com>
2024-06-25 09:22:27 -0700

    C# My Profile Challenge

---
+++ MyProfile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MyProfile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Name: ");
+        string? name = Console.ReadLine();
+        Console.Clear();
+        Console.Write("Enter Age: ");
+        string age = Console.ReadLine();
+         Console.Clear();
+        Console.Write("Enter Your Favorite Color: ");
+        string favoriteColor = Console.ReadLine();
+         Console.Clear();
+
+        Console.WriteLine($"Name: {name}\nAge: {age}\nFavorite Color:\n{favoriteColor}");
+        Console.Read();
+    }
+}
