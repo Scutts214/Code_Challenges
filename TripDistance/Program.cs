2b7e12f1d5c143546f97ef41e942dee3846148e6
Stephen Cutts <sbcutts1981@icloud.com>
2024-06-25 10:32:04 -0700

    C# Challenge Calculate Trip Distance

---
+++ TripDistance/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Calculate Trip Distance");
+Console.Write("Begining Odometer: ");
+int startOdom = Convert.ToInt32(Console.ReadLine());
+Console.Write("End Odometer: ");
+int endOdom = Convert.ToInt32(Console.ReadLine());
+
+var distanceTraveled = endOdom - startOdom;
+Console.WriteLine($"Total Miles Traveled: {distanceTraveled}");
